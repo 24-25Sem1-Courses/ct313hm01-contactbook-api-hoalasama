7b7a462703d7cf9b7c47af92def8c770879e0bf6
hoalasama <hoalasamaeu@gmail.com>
2024-09-18 08:50:00 +0700

    Setup knex.js and insert fake data

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,7 +1,35 @@
+const contactsService = require("../services/contacts.service");
+const ApiError = require("../api-error");
 const JSend = require("../jsend");
-function createContact(req, res) {
-  return res.status(201).json(JSend.success({ contact: {} }));
+
+async function createContact(req, res, next) {
+  if (!req.body?.name || typeof req.body.name !== "string") {
+    return next(new ApiError(400, "Name should be a non-empty string"));
+  }
+
+  try {
+    const contact = await contactsService.createContact({
+      ...req.body,
+      avatar: req.file ? `/public/uploads/${req.file.filename}` : null,
+    });
+    return res
+      .status(201)
+      .set({
+        Location: `${req.baseUrl}/${contact.id}`,
+      })
+      .json(
+        JSend.success({
+          contact,
+        })
+      );
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
 }
+
 function getContactsByFilter(req, res) {
   const filters = [];
   const { favorite, name } = req.query;
